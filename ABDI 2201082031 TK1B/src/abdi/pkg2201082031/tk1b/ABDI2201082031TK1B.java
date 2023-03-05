d27eada8d8c79c433ff8685d6843b6b1c621b83e
irsyad521 <arizonamarco521@gmail.com>
2023-03-05 21:05:57 +0700

    pertemuan 1

---
+++ ABDI 2201082031 TK1B/src/abdi/pkg2201082031/tk1b/ABDI2201082031TK1B.java
@@ -0,0 +1,34 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package abdi.pkg2201082031.tk1b;
+
+/**
+ *
+ * @author LABSIKOMP03
+ */
+public class ABDI2201082031TK1B {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //deklarasi a tipe data dengan nama variable
+        //result dan tipe data boolean
+        boolean result;
+        //deklarasi tipe data nama variable
+        //option dan tipe data char
+        char option;
+        option = 'C';  //menandai 'C' sebagai option
+        
+        //deklarasi tipe data dengan nama variable
+        //grade,double tipe data dan telah di inisialisasi
+        //to 0.0
+        double grade=0.0;
+    }
+    
+}
+  
